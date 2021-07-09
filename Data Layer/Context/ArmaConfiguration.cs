16136247ae08b2ec5ac1f9097274dcf1a695a8e7
Eleonora Lombardo (c) <eleonora.lombardo@avanade.com>
2021-07-09 11:33:10 +0200

    11:33

--- Data Layer/Context/ArmaConfiguration.cs
+++ Data Layer/Context/ArmaConfiguration.cs
@@ -17,6 +17,81 @@ namespace Data_Layer.Context
             builder.Property(k => k.Nome).IsRequired();
             builder.Property(k => k.Danno).IsRequired();
             builder.Property(k => k.TipoPersonaggio).IsRequired();
+            builder.HasData(
+                new Arma
+                {
+                    id=1,
+                    Nome="Ascia",
+                    Danno=8,
+                    TipoPersonaggio="Soldier"
+                }, new Arma
+                {
+                    id = 2,
+                    Nome = "Mazza",
+                    Danno = 5,
+                    TipoPersonaggio = "Soldier"
+                },new Arma
+                {
+                    id = 3,
+                    Nome = "Spada",
+                    Danno = 10,
+                    TipoPersonaggio = "Soldier"
+                }, new Arma
+                {
+                    id = 4,
+                    Nome = "Arco e frecce",
+                    Danno = 8,
+                    TipoPersonaggio = "Wizard"
+                }, new Arma
+                {
+                    id = 5,
+                    Nome = "Bacchetta",
+                    Danno = 5,
+                    TipoPersonaggio = "Wizard"
+                }, new Arma
+                {
+                    id = 6,
+                    Nome = "Bastone Magico",
+                    Danno = 10,
+                    TipoPersonaggio = "Wizard"
+                }, new Arma
+                {
+                    id = 7,
+                    Nome = "Arco",
+                    Danno = 7,
+                    TipoPersonaggio = "Ghost"
+                }, new Arma
+                {
+                    id = 8,
+                    Nome = "Clava",
+                    Danno = 5,
+                    TipoPersonaggio = "Ghost"
+                }, new Arma
+                {
+                    id = 9,
+                    Nome = "Divinazione",
+                    Danno = 15,
+                    TipoPersonaggio = "Lucifer"
+                }, new Arma
+                {
+                    id = 10,
+                    Nome = "Fulmine",
+                    Danno = 10,
+                    TipoPersonaggio = "Lucifer"
+                }, new Arma
+                {
+                    id = 11,
+                    Nome = "Tempesta",
+                    Danno = 8,
+                    TipoPersonaggio = "Lucifer"
+                }, new Arma
+                {
+                    id = 12,
+                    Nome = "Tempesta oscura",
+                    Danno = 15,
+                    TipoPersonaggio = "Lucifer"
+                }
+            );
         }
     }
 }
--- Data Layer/Context/MostroConfiguration.cs
+++ Data Layer/Context/MostroConfiguration.cs
@@ -11,11 +11,26 @@ namespace Data_Layer.Context
 {
     public class MostroConfiguration : IEntityTypeConfiguration<Mostro>
     {
+        Random r = new Random();
         public void Configure(EntityTypeBuilder<Mostro> builder)
         {
             builder.HasKey(k => k.id);
             builder.Property(k => k.Categoria).IsRequired();
             builder.HasOne(k => k.arma).WithMany(c => c.mostri).HasForeignKey(k => k.idArma);
+            builder.HasData(
+                new Mostro
+                {
+                    id = 1,
+                    Categoria = "Lucifer",
+                    idArma = r.Next(9, 12)
+                },
+                new Mostro
+                {
+                    id = 2,
+                    Categoria = "Ghost",
+                    idArma = r.Next(7, 8)
+                }
+            ); 
         }
     }
 }
